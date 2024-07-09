08e0d861d67270ff243774396f24745488f942a6
Cheer Chokwittaya <chokwittaya.nutt@gmail.com>
2024-07-09 16:19:24 -0500

    [11] - use Interface with Classes

--- src/12-classes.ts
+++ src/12-classes.ts
@@ -1,4 +1,39 @@
 import Invoice from './classes/Invoice.js';
+import Payment from './classes/Payment.js';
+import HasFormatter from './interfaces/HasFormatter.js';
+
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice('yoshi', 'web work', 250);
+const invoiceOne: HasFormatter = new Invoice('yoshi', 'web work', 250);
+docTwo = new Payment('mario', 'plumbing work', 200);
+
+const docs: HasFormatter[] = [];
+docs.push(docOne);
+docs.push(docTwo);
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toForm = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(toForm.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(toForm.value, details.value, amount.valueAsNumber);
+  }
+  console.dir(doc);
+});
+
+// -----------------------------
 
 const invOne = new Invoice('mario', 'work on the mario website', 200);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,11 +1,15 @@
 // classes
-class Invoice {
+
+import HasFormatter from '../interfaces/HasFormatter.js';
+
+class Invoice implements HasFormatter {
   constructor(
     readonly client: string,
     private _details: string,
     public amount: number
   ) {}
 
+  // this method must be implemented because of the HasFormatter interface
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}`;
   }
