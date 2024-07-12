e34d15d83532334a3e143d9cb7adda829b655130
Cheer Chokwittaya <chokwittaya.nutt@gmail.com>
2024-07-12 00:40:41 -0500

    [16] - More about Generic

--- src/16-generic.ts
+++ src/16-generic.ts
@@ -42,3 +42,84 @@ const docFive: Resource<string[]> = {
 };
 
 console.log(docThree, docFour, docFive);
+
+// MORE GENERIC
+
+const getFirstElement = <T>(array: T[]) => {
+  return array[0];
+};
+
+const numbers = [1, 2, 3];
+const firstNum = getFirstElement(numbers);
+
+const strings = ['aaa', 'bbb'];
+const firstString = getFirstElement(strings);
+
+const input = document.querySelector<HTMLInputElement>('.input');
+
+const a = [1, 2, 3];
+a.map(() => {
+  return '';
+});
+
+const map = new Map([['ava', 3]]);
+const map2 = new Map<string, Map<string, number>>();
+
+type ApiResponse<Data> = {
+  data: Data;
+  isError: boolean;
+};
+
+type ApiResponseWithDefault<Data = { status: number }> = {
+  data: Data;
+  isError: boolean;
+};
+
+type UserResponse = ApiResponse<{ name: string; age: number }>;
+type BlogResponse = ApiResponse<{ name: string; publishDate: string }>;
+type StatusResponse = ApiResponse<{ status: number }>;
+
+const response2: UserResponse = {
+  data: {
+    name: 'Sam',
+    age: 50,
+  },
+  isError: true,
+};
+
+const response3: BlogResponse = {
+  data: {
+    name: 'Sam',
+    publishDate: '1234',
+  },
+  isError: true,
+};
+
+// more about default and extend the generic
+
+const response: ApiResponse<{ name: string; age: number }> = {
+  data: {
+    name: 'Sam',
+    age: 50,
+  },
+  isError: true,
+};
+
+const response4: ApiResponseWithDefault = {
+  data: {
+    status: 200,
+  },
+  isError: false,
+};
+
+type ApiResponseWithExtendsAndDefault<
+  Data extends object = { status: number }
+> = {
+  data: Data;
+  isError: boolean;
+};
+
+const response5: ApiResponseWithExtendsAndDefault = {
+  data: { status: 200 },
+  isError: false,
+};
